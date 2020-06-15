cb7914e8299dff571470a0e5573c0e683a5942cc
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-06-15 16:04:36 +0200

    Revert "Properly crop data to "core" Oceania (#6)"
    This reverts commit b9822179aa01954750593efbc2ef92311cb67e11.

--- map-Schoenus-species-richness-AUS-NZL.R
+++ map-Schoenus-species-richness-AUS-NZL.R
@@ -33,8 +33,9 @@ border <- rbind(border_AUS, border_NZL)
 # Lots of stray islands...
 
 # Crop to focus on the mainlands (NOTE: takes a while)
-main_extent <-extent(110, 180, -50, 0)
-border_cropped <- crop(border, main_extent)
+border_cropped <- crop(border, QDGC)
+# Have a look
+#plot(border_cropped)
 
 # Tidy data --------------------------------------------------------------------
 
@@ -75,10 +76,7 @@ occ_spdf$QDGC <- occ_spdf %>%
   pull(qdgc) %>%
   as.character()
 
-# Crop to focus on the mainlands (NOTE: takes a while)
-occ_spdf_cropped <- crop(occ_spdf, main_extent)
-
-richness <- occ_spdf_cropped %>%
+richness <- occ_spdf %>%
   as_tibble() %>%
   group_by(QDGC) %>%
   summarise(richness = length(unique(species))) %>%
@@ -114,7 +112,7 @@ richness_map <- ggplot(richness) +
   aes(QDGC_lon, QDGC_lat, fill = richness) +
   geom_tile() +
   geom_polygon(
-    data = border_cropped,
+    data = border,
     aes(x = long, y = lat, group = group),
     colour = "black", fill = NA, size = 0.25
   ) +
