5b84df6ad29adafdb300a8ef1696fb43c4cfcc15
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-06-30 12:49:15 +0200

    Update paths for saved figures

--- R/figure-1-maps.R
+++ R/figure-1-maps.R
@@ -6,13 +6,13 @@ source("R/map-Schoenus-richness-South-Africa.R")
 both_plots <- south_africa_plot + oceania_plot
 
 ggsave(
-  "figure-1-maps.pdf",
+  "figures/01-maps.pdf",
   both_plots,
   width = 10, height = 4
 )
 
 ggsave(
-  "figure-1-maps.png",
+  "figures/01-maps.png",
   both_plots,
   width = 10, height = 4, dpi = 300
 )
