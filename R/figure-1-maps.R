97763c9e9c7de00a26a14e7a849c123b23e9e27c
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-07-01 18:32:36 +0200

    Update script to integrate maps

--- R/figure-1-maps.R
+++ R/figure-1-maps.R
@@ -2,19 +2,37 @@
 
 library(patchwork)  # Figure panelling
 
-source("R/map-Schoenus-richness-Oceania.R")
+source("R/map-Schoenus-worldwide.R")
 source("R/map-Schoenus-richness-South-Africa.R")
+source("R/map-Schoenus-richness-Oceania.R")
 
-both_plots <- south_africa_plot + oceania_plot
+richness_maps <-
+  worldwide_plot +
+  (south_africa_plot | oceania_plot) +
+  plot_layout(nrow = 2, heights = c(1, 0.75))
 
 ggsave(
   "figures/01-maps.pdf",
-  both_plots,
-  width = 10, height = 4
+  richness_maps,
+  width = 10, height = 7
 )
 
 ggsave(
   "figures/01-maps.png",
-  both_plots,
-  width = 10, height = 4, dpi = 300
+  richness_maps,
+  width = 10, height = 7, dpi = 300
+)
+
+sampling_maps <- n_spp_map / p_spp_map
+
+ggsave(
+  "figures/01-sampling.pdf",
+  sampling_maps,
+  width = 10, height = 7
+)
+
+ggsave(
+  "figures/01-sampling.png",
+  sampling_maps,
+  width = 10, height = 7, dpi = 300
 )
