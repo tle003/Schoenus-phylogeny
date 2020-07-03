b26ad8c41e1ca85712d37fe7f915bb72208707af
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-07-03 18:11:31 +0200

    Add South Africa border as well, to show sampling only within country

--- R/map-Schoenus-richness-South-Africa.R
+++ R/map-Schoenus-richness-South-Africa.R
@@ -14,6 +14,9 @@ library(tidyverse)  # Data wrangling, figures
 # South African Schoenus richness in QDGC (from Tammy Elliott)
 richness <- read_csv("data/Schoenus-South-Africa-richness.csv")
 
+# South African border
+border_ZAF <- getData("GADM", country = "ZAF", level = 0)
+
 # African continental border
 border_world<- readOGR("data/shapefiles/World_Continents/v10/continent.gdb/")
 # (Will zoom in on southern Africa in ggplot2:: code below)
@@ -28,6 +31,11 @@ south_africa_plot <- ggplot(richness) +
   geom_tile() +
   geom_polygon(
     data = border_world,
+    aes(x = long, y = lat, group = group),
+    fill = NA, colour = "grey30", size = 0.15
+  ) +
+  geom_polygon(
+    data = border_ZAF,
     aes(x = long, y = lat, group = group),
     fill = NA, colour = "grey30", size = 0.15
   ) +
