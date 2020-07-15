d21f6347f92291d6a2cf0564ba76a759cb621b16
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-07-15 15:11:31 +0200

    Remove binned BS coloured tree

--- R/figure-2-phylogenies.R
+++ R/figure-2-phylogenies.R
@@ -32,38 +32,6 @@ Schoenus_BS_plot <- ggtree(Schoenus, ladderize = TRUE, right = TRUE, root.positi
   ) +
   theme(legend.position = c(0.9, 0.15), legend.text.align = 1)
 
-Schoenus_simpler <- Schoenus
-Schoenus_simpler$node.label <- Schoenus_simpler$node.label %>%
-  as.numeric() %>%
-  {case_when(
-    . == 100 ~   "100",
-    . >=  90 ~ ">= 90",
-    . >=  80 ~ ">= 80",
-    . >=  70 ~ ">= 70",
-    . >=  60 ~ ">= 60",
-    . >=  50 ~ ">= 50",
-    TRUE     ~  "< 50"
-  )}
-
-Schoenus_simpler_BS_plot <- ggtree(Schoenus_simpler, ladderize = TRUE, right = TRUE, root.position = 0.01) +
-  geom_rootedge(rootedge = 0.01) +
-  xlim(0, 0.33) +
-  geom_tiplab(aes(label = paste0('italic(\"', label, '\")')), parse = TRUE, size = 2.5) +
-  geom_nodepoint(
-    aes(fill = factor(label, levels = c(
-        "100",
-      ">= 90",
-      ">= 80",
-      ">= 70",
-      ">= 60",
-      ">= 50",
-       "< 50"
-    ))),
-    pch = 21, size = 2
-  ) +
-  scale_fill_grey(name = "BS (%)", start = 0, end = 1) +
-  theme(legend.position = c(0.9, 0.15))
-
 tree_sample <- read.tree("data/phylogenies/2020-07-14_RAxML-HPC-reconstruction_04/RAxML_bootstrap.result")
 
 set.seed(1234)
