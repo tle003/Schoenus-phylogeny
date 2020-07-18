c8be9fcf3e1d4e134b3402bec339e8bdcb317ea8
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-07-18 22:10:32 +0200

    Add more WIP/fiddling w/ dispersal matrices

--- R/figure-3-dispersal-maps.R
+++ R/figure-3-dispersal-maps.R
@@ -4,6 +4,7 @@
 
 # Load packages ----------------------------------------------------------------
 
+library(rgdal) # Shapefile I/O
 library(tidyverse)
 
 # Import data ------------------------------------------------------------------
@@ -11,14 +12,40 @@ library(tidyverse)
 dispersal_matrix <- read_tsv("data/all_dispersals_counts_fromto_means.txt")
 dispersal_matrix_codes <- read_csv("data/all_dispersals_counts_fromto_means_codes.csv")
 
+TDWG <- readOGR("~/wgsrpd/level1", layer = "level1", stringsAsFactors = FALSE)
+
+plot(TDWG$LEVEL1_NAM)
+
 theme_set(theme_classic())
 dispersal_matrix %>%
-  gather(to, rate, -X1) %>%
+  gather(to, mean_n_dispersal_events, -X1) %>%
   rename(from = X1) %>%
   mutate_at(vars(c("from", "to")), ~map_chr(.,
     ~dispersal_matrix_codes$region[dispersal_matrix_codes$code == .]
   )) %>%
-  filter(rate > 0) %>%
+  filter(mean_n_dispersal_events > 0) %>%
   ggplot() +
-    aes(x = to, y = from, size = rate) +
+    aes(x = to, y = from, size = mean_n_dispersal_events) +
     geom_point()
+
+dispersal_matrix %>%
+  {
+    y <- as.matrix(.[, -1])
+    rownames(y) <- .[[1]]
+    y
+  } %>%
+  t() %>%
+  round() %>%
+  diagram::plotmat()
+
+library(network)
+devtools::install_github("briatte/ggnet")
+library(ggnet)
+dispersal_matrix %>%
+  {
+    y <- as.matrix(.[, -1])
+    rownames(y) <- .[[1]]
+    y
+  } %>%
+  network(directed = TRUE) %>%
+  ggnet2(label = "vertex.names")
