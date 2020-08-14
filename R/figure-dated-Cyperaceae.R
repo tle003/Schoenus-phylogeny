eac36b86fc7a5fbe15b8ce69cb2bf2a41652f84b
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-08-14 15:01:47 +0200

    Fix backwards node HPDs by plotting tree right-to-left

--- R/figure-dated-Cyperaceae.R
+++ R/figure-dated-Cyperaceae.R
@@ -36,32 +36,49 @@ tree_height <- max(nodeHeights(MCC_tree@phylo))
 my_labels <- c(90, 80, 70, 60, 50, 40, 30, 20, 10, 0)
 label_positions <- tree_height - my_labels
 
+# Try and fix node HPDs plotting backwards
 nodes_with_future_HPDs <- MCC_tree@data %>%
   select(node, height_0.95_HPD) %>%
   unnest() %>%
   filter(height_0.95_HPD > tree_height) %>%
   pull(node)
-
 MCC_tree@data %>%
   filter(node %in% nodes_with_future_HPDs) %>%
-  unnest()
+  as.data.frame()
+
+# This doesn't work:
+#MCC_tree@data <- MCC_tree@data %>%
+#  mutate(height_0.95_HPD2 = purrr::map(height_0.95_HPD, rev))
+
+# Neither does this:
+#MCC_tree@data$height_0.95_HPD <- MCC_tree@data$height_0.95_HPD %>%
+#  purrr::map(rev)
+#  purrr::map(~{. - tree_height})
+
+# Temporary solution used below:
+# plot tree right-to-left instead of left-to-right
+# (Don't know why, but this seems to work best)
+
+# Alternatives:
+# - FigTree
+# - phytools:: + base::
 
 Cyperaceae_tree_plot <- ggtree(MCC_tree) +
   geom_tiplab(
     aes(label = paste0('italic(\"', label, '\")')),
     parse = TRUE,
     size = 2.5,
-    offset = 10
+    offset = -40
   ) +
-  geom_range("height_0.95_HPD",
+  geom_range("height_0.95_HPD2",
     center = "height_median",
     size   = 1.5,
     alpha  = 0.33
   ) +
   #geom_hilight(Schoenus_MRCA_node,  fill = "darkblue",  alpha = 0.25) +
-  #geom_hilight(Schoeneae_MRCA_node, fill = "blue", alpha = 0.25) +
-  scale_x_continuous(name = "Ma",
-    limits = c(0, 150),
+  #geom_hilight(Schoeneae_MRCA_node, fill = "lightblue", alpha = 0.25) +
+  scale_x_reverse(name = "Ma",
+    limits = c(135, -10),
     breaks = label_positions,
     labels = my_labels
   ) +
