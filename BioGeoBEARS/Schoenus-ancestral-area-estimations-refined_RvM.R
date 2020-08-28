ab9e17363ba1a353160960f343417a6947c47687
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-08-28 12:48:42 +0200

    Copy parts of script I'm running into own script, just in case

---
+++ BioGeoBEARS/Schoenus-ancestral-area-estimations-refined_RvM.R
@@ -0,0 +1,96 @@
+# [RvM] Setup ------------------------------------------------------------------
+
+setwd("BioGeoBEARS")  # [Rvm]
+
+# Load the required libraries.
+#install.packages("rexpokit")
+#install.packages("cladoRcpp")
+#install.packages("devtools")
+#library(devtools)
+#devtools::install_github(repo="nmatzke/BioGeoBEARS")
+# Load the package (after installation, see above).
+library(GenSA)    # GenSA is better than optimx (although somewhat slower)
+library(FD)       # for FD::maxent() (make sure this is up-to-date)
+library(snow)     # (if you want to use multicore functionality; some systems/R versions prefer library(parallel), try either)
+library(parallel)
+library(rexpokit)
+library(cladoRcpp)
+library(devtools)
+library(BioGeoBEARS)
+library(ape)
+library(FossilSim)
+library(phangorn)
+library(phytools)
+
+# [RvM] BSM analysis -----------------------------------------------------------
+
+######################################################
+# Get the inputs for Biogeographical Stochastic Mapping
+# Note: this can be slow for large state spaces and trees, since
+# the independent likelihoods for each branch are being pre-calculated
+# E.g., for 10 areas, this requires calculation of a 1024x1024 matrix
+# for each branch.  On a tree with ~800 tips and thus ~1600 branches, this was about 1.6 gigs
+# for storage of "BSM_inputs_file.Rdata".
+# Update: 2015-09-23 -- now, if you used multicore functionality for the ML analysis,
+# the same settings will be used for get_inputs_for_stochastic_mapping().
+#######################################################
+#Read in data
+results_DEC<-readRDS(file = "results_DEC_constrained.rds")
+results_DEC$inputs$wd <- getwd()  # [Rvm]
+results_DEC$inputs$geogfn <- "Schoeneae-DEC-9areas.txt"  # [Rvm]
+results_DEC$inputs$trfn <- "Schoeneae_tree_ultrametric.tre"  # [Rvm]
+res<-results_DEC
+
+tree_file_name <- np("Schoeneae_tree_ultrametric.tre")
+tree_file_name
+
+tr <- read.tree(tree_file_name)
+
+#extdata loaad
+extdata_dir = np(system.file("extdata", package="BioGeoBEARS"))
+extdata_dir
+
+geo_file_name <- np("Schoeneae-DEC-9areas.txt")
+tipranges <- getranges_from_LagrangePHYLIP(lgdata_fn=geo_file_name)
+tipranges
+
+
+
+BSM_inputs_fn = "BSM_inputs_file.Rdata"
+runInputsSlow = TRUE
+if (runInputsSlow)
+    {
+    stochastic_mapping_inputs_list = get_inputs_for_stochastic_mapping(res=res)
+    save(stochastic_mapping_inputs_list, file=BSM_inputs_fn)
+    } else {
+    # Loads to "stochastic_mapping_inputs_list"
+    load(BSM_inputs_fn)
+    } # END if (runInputsSlow)
+
+# Check inputs (doesn't work the same on unconstr)
+names(stochastic_mapping_inputs_list)
+stochastic_mapping_inputs_list$phy2
+stochastic_mapping_inputs_list$COO_weights_columnar
+stochastic_mapping_inputs_list$unconstr
+set.seed(seed=as.numeric(Sys.time()))
+
+runBSMslow = TRUE
+if (runBSMslow == TRUE)
+    {
+    # Saves to: RES_clado_events_tables.Rdata
+    # Saves to: RES_ana_events_tables.Rdata
+    BSM_output = runBSM(res, stochastic_mapping_inputs_list=stochastic_mapping_inputs_list, maxnum_maps_to_try=150, nummaps_goal=100, maxtries_per_branch=40000, save_after_every_try=TRUE, savedir=getwd(), seedval=12345, wait_before_save=0.01)
+
+    RES_clado_events_tables = BSM_output$RES_clado_events_tables
+    RES_ana_events_tables = BSM_output$RES_ana_events_tables
+    } else {
+    # Load previously saved...
+
+    # Loads to: RES_clado_events_tables
+    load(file="RES_clado_events_tables.Rdata")
+    # Loads to: RES_ana_events_tables
+    load(file="RES_ana_events_tables.Rdata")
+    BSM_output = NULL
+    BSM_output$RES_clado_events_tables = RES_clado_events_tables
+    BSM_output$RES_ana_events_tables = RES_ana_events_tables
+    } # END if (runBSMslow == TRUE)
