3dfdab990d56fb5fd26e9c84380afc14ea404b3d
Ruan van Mazijk <ruanvmazijk@gmail.com>
2020-08-28 15:25:39 +0200

    Copy more parts of script I ran

--- BioGeoBEARS/Schoenus-ancestral-area-estimations-refined_RvM.R
+++ BioGeoBEARS/Schoenus-ancestral-area-estimations-refined_RvM.R
@@ -94,3 +94,49 @@ if (runBSMslow == TRUE)
     BSM_output$RES_clado_events_tables = RES_clado_events_tables
     BSM_output$RES_ana_events_tables = RES_ana_events_tables
     } # END if (runBSMslow == TRUE)
+
+
+
+
+# Extract BSM output
+clado_events_tables = BSM_output$RES_clado_events_tables
+ana_events_tables = BSM_output$RES_ana_events_tables
+head(clado_events_tables[[1]])
+head(ana_events_tables[[1]])
+length(clado_events_tables)
+length(ana_events_tables)
+
+
+include_null_range = TRUE
+areanames = names(tipranges@df)
+areas = areanames
+max_range_size = 6
+
+# Note: If you did something to change the states_list from the default given the number of areas, you would
+# have to manually make that change here as well! (e.g., areas_allowed matrix, or manual reduction of the states_list)
+states_list_0based = rcpp_areas_list_to_states_list(areas=areas, maxareas=6, include_null_range=include_null_range)
+
+colors_list_for_states = get_colors_for_states_list_0based(areanames=areanames, states_list_0based=states_list_0based, max_range_size=5, plot_null_range=TRUE)
+
+
+
+############################################
+# Setup for painting a single stochastic map
+############################################
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+stratified = TRUE
+clado_events_table = clado_events_tables[[1]]
+ana_events_table = ana_events_tables[[1]]
+
+# [RvM] BSM results ------------------------------------------------------------
+
+# Convert the BSM into a modified res object
+master_table_cladogenetic_events = clado_events_tables[[1]]
+resmod = stochastic_map_states_into_res(res=res, master_table_cladogenetic_events=master_table_cladogenetic_events, stratified=stratified)
+
+plot_BioGeoBEARS_results(results_object=resmod, analysis_titletxt="Stochastic map", addl_params=list("j"), label.offset=0.5, plotwhat="text", cornercoords_loc=scriptdir, root.edge=TRUE, colors_list_for_states=colors_list_for_states, skiptree=FALSE, show.tip.label=TRUE)
+
+# Paint on the branch states
+paint_stochastic_map_branches(res=resmod, master_table_cladogenetic_events=master_table_cladogenetic_events, colors_list_for_states=colors_list_for_states, lwd=4, lty=par("lty"), root.edge=TRUE, stratified=stratified)
+
+plot_BioGeoBEARS_results(results_object=resmod, analysis_titletxt="Stochastic map", addl_params=list("j"), plotwhat="text", cornercoords_loc=scriptdir, root.edge=TRUE, colors_list_for_states=colors_list_for_states, skiptree=TRUE, show.tip.label=TRUE)
