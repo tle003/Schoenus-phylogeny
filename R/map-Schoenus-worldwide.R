ed4ececc75ff189ad0e298c83315733e1f9509be
Ruan van Mazijk <ruanvmazijk@gmail.com>
2021-01-28 14:39:57 +0200

    Keep S. trachycarpus (for now?)

--- R/map-Schoenus-worldwide.R
+++ R/map-Schoenus-worldwide.R
@@ -23,7 +23,6 @@ colnames(Schoenus_worldwide)[1:4] <- c(
   "source",
   "in_phylogeny"
 )
-Schoenus_worldwide <- Schoenus_worldwide[Schoenus_worldwide$species != "Schoenus trachycarpus", ]
 
 # Give species names as rownames
 rownames(Schoenus_worldwide) <- Schoenus_worldwide$species
