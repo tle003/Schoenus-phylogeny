32f0ce5c942d9466a605cfcce083e06de0fb4cca
Ruan van Mazijk <ruanvmazijk@gmail.com>
2021-02-11 15:41:05 +0200

    Update filenames in R-script

--- R/figure-4-phylogenies.R
+++ R/figure-4-phylogenies.R
@@ -15,8 +15,8 @@ library(lemon)      # For ::coord_capped_cart()
 
 # Import data ------------------------------------------------------------------
 
-MCC_tree <- treeio::read.beast("data/phylogenies/Cyperaceae-all-taxa-6-calib-st6.tre")
-posterior_sample <- read.tree("data/phylogenies/Cyperaceae.trees.100.trees")
+MCC_tree <- treeio::read.beast("data/phylogenies/Cyperaceae-all-taxa-6-calib-3-5gene-lognormaldist-newsub-Dec27.tre")
+posterior_sample <- read.tree("data/phylogenies/Schoenus.trees.100.trees")
 
 # Tidy data --------------------------------------------------------------------
 
